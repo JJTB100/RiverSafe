e3ad35f4e261067308d219c6d5dae3b02b901c8b
JJTB100 <17brostj@fulford.york.sch.uk>
2022-03-04 14:20:57 +0000

    Read values from sensor

--- python/server.py
+++ python/server.py
@@ -1,5 +1,17 @@
 import serial
+import re
+
+# connect to serial port
 s = serial.Serial(port="/dev/ttyACM0", baudrate=115200,timeout=1)
+FILENAME = "/home/riversafe/RiverSafe/web/tds.js"
+
+# keep reading sensor value from pico
 while True:
  line = str(s.readline())
- print(line)
+ match = re.search("b'(\d+)", line)
+ if match: 
+  f = open(FILENAME, "w")
+  value = match.group(1)
+  f.write(value)
+  f.close()
+  
