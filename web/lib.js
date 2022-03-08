4f1fc928bccc3c2e96e751a782ace2dd695286d7
Jonathan Broster <17BrostJ@fulford.york.sch.uk>
2022-03-08 11:15:21 +0000

    Working temp gauge?(!)

--- web/lib.js
+++ web/lib.js
@@ -2,22 +2,36 @@ google.charts.load('current', { 'packages': ['gauge', 'corechart'] });
 google.charts.setOnLoadCallback(init);
 window.onresize=init;
 
-var gaugeOptions = {
+var gaugeOptionsTDS = {
     min: 0, max: 600, yellowFrom: 300, yellowTo: 400,
     greenFrom: 0, greenTo:300,
     redFrom: 400, redTo: 600, minorTicks: 5
-
 };
-var gauge;
+
+var gaugeOptionsTmp = {
+    min: -10, max: 100
+};
+var gaugeTDS;
+var gaugeTemp;
+var guageDataTDS;
+var guageDataTmp;
 
 function drawGauge() {
-    gaugeData = new google.visualization.DataTable();
-    gaugeData.addColumn('number', 'Pollution');
-    gaugeData.addRows(2);
-    gaugeData.setCell(0, 0, 50);
+    gaugeDataTDS = new google.visualization.DataTable();
+    gaugeDataTDS.addColumn('number', 'Pollution');
+    gaugeDataTDS.addRows(2);
+    gaugeDataTDS.setCell(0, 0, 50);
 
-    gauge = new google.visualization.Gauge(document.getElementById('gauge_div'));
-    gauge.draw(gaugeData, gaugeOptions);
+    gaugeTDS = new google.visualization.Gauge(document.getElementById('gauge_tds_div'));
+    gaugeTDS.draw(gaugeDataTDS, gaugeOptionsTDS);
+
+    gaugeDataTmp = new google.visualization.DataTable();
+    gaugeDataTmp.addColumn('number', 'Temperature');
+    gaugeDataTmp.addRows(2);
+    gaugeDataTmp.setCell(0, 0, 25);
+
+    gaugeTemp = new google.visualization.Gauge(document.getElementById('gauge_temp_div'));
+    gaugeTemp.draw(gaugeDataTmp, gaugeOptionsTmp);
 }
 
 function drawChart() {
@@ -41,10 +55,6 @@ function drawChart() {
     chart.draw(data, options);
   }
 
-function changeTemp(dir) {
-    gaugeData.setValue(0, 0, gaugeData.getValue(0, 0) + dir * 25);
-    gauge.draw(gaugeData, gaugeOptions);
-}
 
 function updateValue() {
     //console.log("updating");
@@ -54,8 +64,10 @@ function updateValue() {
         dataType: "json",
         success: function(data) {
             console.log(data);
-            gaugeData.setValue(0, 0, data.tds);
-            gauge.draw(gaugeData, gaugeOptions);
+            gaugeDataTDS.setValue(0, 0, data.tds);
+            gaugeTDS.draw(gaugeDataTDS, gaugeOptionsTDS);
+            gaugeDataTmp.setValue(0, 0, data.temp);
+            gaugeTemp.draw(gaugeDataTmp, gaugeOptionsTmp);
             setTimeout(updateValue, 2000);    
         }
     });
